c61a542218c38c70a1bce7ce30b9c1ac3d30b39d
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-01-08 20:01:39 +0200

    Added input prompt

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Enter your choice: R(ock), P(aper) or S(cissors).");
-    }
-}
---
+++ src/rps/GameController.java
@@ -0,0 +1,12 @@
+package rps;
+
+public class GameController {
+
+    public String welcomeMessage(){return "Please choose R(ock), P(aper) or S(cissors)";}
+
+
+
+
+
+
+}
